0b13f7d91e9b4ec19376aeec0a156065ce011f10
gsha <girishsharma001@gmail.com>
2018-10-01 15:10:06 +0530

    Add: start page url from config.ini

--- creator.py
+++ creator.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from base import *
 import unittest
-
+import configparser
 
 class IssueCreator(unittest.TestCase):
 
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.config = configparser.ConfigParser()
+        self.config.read("config.ini")
+        self.start_page_url = self.config.get('URL', 'START_PAGE_URL')
 
     def create_issue(self):
-        self.driver.get('https://github.com/ZoranPandovski/'
-                        'al-go-rithms/issues')
+        self.driver.get(self.start_page_url)
         login = LoginPage(self.driver)
         login.fill_sign_in()
         ci = IssuesPage(self.driver)
