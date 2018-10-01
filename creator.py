e08d9ee0e0f37964cfa1efc7802b385c18d3c911
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-10-01 13:43:49 -0300

    get url from congi file

--- creator.py
+++ creator.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from base import *
 import unittest
-
+import ConfigParser
 
 class IssueCreator(unittest.TestCase):
 
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.config = ConfigParser.ConfigParser()
+        self.config.read("config.ini")
+        self.issuesURL = self.config.get('ISSUES', 'URL')
 
     def create_issue(self):
-        self.driver.get('https://github.com/ZoranPandovski/'
-                        'al-go-rithms/issues')
+        self.driver.get(self.issuesURL)
         login = LoginPage(self.driver)
         login.fill_sign_in()
         ci = IssuesPage(self.driver)
