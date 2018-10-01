cc9be693ec5c5a0a7d293a2c2bbd1466d7fc87de
hecris <christopher.he33@myhunter.cuny.edu>
2018-10-01 11:26:15 -0400

    added URL to config.ini and changed creator.py to read url from config.ini

--- creator.py
+++ creator.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from base import *
 import unittest
+import ConfigParser
 
 
 class IssueCreator(unittest.TestCase):
 
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.config = ConfigParser.ConfigParser()
+        self.config.read("config.ini")
 
     def create_issue(self):
-        self.driver.get('https://github.com/ZoranPandovski/'
-                        'al-go-rithms/issues')
+        self.issues_url = self.config.get('ISSUES', 'URL')
+        self.driver.get(issues_url)
         login = LoginPage(self.driver)
         login.fill_sign_in()
         ci = IssuesPage(self.driver)
