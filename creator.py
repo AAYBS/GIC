eb6f41089e3b8f56a8f13daaf7bcf9834ff78213
Aram <aramharoyan@gmail.com>
2018-10-01 17:34:27 +0400

    Update creator.py

--- creator.py
+++ creator.py
@@ -1,22 +1,30 @@
-from selenium import webdriver
-from base import *
+import configparser
 import unittest
+
+from selenium import webdriver
+
+from base import *
 
 
 class IssueCreator(unittest.TestCase):
 
     def __init__(self):
+        self.config = configparser.ConfigParser()
         self.driver = webdriver.Firefox()
 
     def create_issue(self):
-        self.driver.get('https://github.com/ZoranPandovski/'
-                        'al-go-rithms/issues')
+        self.driver.get(self.get_url_from_config())
         login = LoginPage(self.driver)
         login.fill_sign_in()
         ci = IssuesPage(self.driver)
         ci.create_issue()
         #close browser
         ci.close()
+
+    def get_url_from_config(self):
+        self.config.read('config.ini')
+        return self.config['URL']['DEFAULT_URL']
+
 
 if __name__ == "__main__":
     ic = IssueCreator()
